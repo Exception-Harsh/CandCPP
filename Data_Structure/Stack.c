447faeaab408824964fdfefa99c3863a8ffabb87
Exception-Harsh <harsh180403singh@gmail.com>
2022-09-29 20:15:24 +0530

    commit

---
+++ Data_Structure/Stack.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+int stack[100], top, maxSize, i;
+
+void display();
+void pop();
+void push();
+
+int main()
+{
+    top = -1;
+    printf("Enter size of stack: ");
+    scanf("%d", &maxSize);
+    stack[maxSize];
+    int choice;
+
+    printf("*************MENU**************\n");
+    printf("\n\t 1: Push\n\t 2: Pop\n\t 3: Display\n\t 4: exit\n");
+
+    do
+    {
+        printf("\nEnter Choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            push();
+            break;
+
+        case 2:
+            pop();
+            break;
+
+        case 3:
+            display();
+            break;
+
+        default:
+            printf("Wrong choice! Try Again");
+            break;
+        }
+    } while (choice != 4);
+    return 0;
+}
+
+void push()
+{
+    int data;
+    if (top < maxSize - 1)
+    {
+        printf("Enter Element to be pushed in Stack: ");
+        scanf("%d", &data);
+        top++;
+        stack[top] = data;
+    }
+    else
+    {
+        printf("Stack is already full\n");
+    }
+}
+
+void pop()
+{
+    if (top > -1)
+    {
+
+        printf("%d is removed from stack ", stack[top]);
+        top--;
+    }
+    else
+        printf("Stack is Empty\n");
+}
+
+void display()
+{
+    if (top >= 0)
+    {
+        printf("\n The elements in STACK \n");
+        for (i = top; i >= 0; i--)
+            printf("\n%d", stack[i]);
+        printf("\n Press Next Choice");
+    }
+    else
+    {
+        printf("\n The STACK is empty");
+    }
+}
